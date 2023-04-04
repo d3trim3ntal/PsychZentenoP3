c6457db8647b04d1973a71082be18068248c687b
d3trim3ntal <obsidianblackrose@gmail.com>
2023-04-03 22:15:46 -0700

    Updated PlayerController Script
    Added constraints and player movement. Also organized hierarchy and script.

--- PsychZentenoP3Final/Assets/PlayerController.cs
+++ PsychZentenoP3Final/Assets/PlayerController.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private float speed = 10.0f;
+    private float zBound = 24;
+    private Rigidbody playerRb;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRb = <GetComponent>Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        MovePlayer();
+        ConstrainPlayerPosition();
+    }
+
+    // Moves the player based on arrow key Input
+    void MovePlayer()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        playerRb.AddForce(Vector3.forward * speed * verticalInput);
+        playerRb.AddForce(Vector3.right * speed * horizontalInput);       
+    }
+
+    // Prevent the player from leaving the top or bottom of the screen
+    void ConstrainPlayerPosition()
+    {
+        if (transform.position.z < zBound)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -zBound);
+        }
+
+        if (transform.position = > zBound)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zBound);
+        }
     }
 }
